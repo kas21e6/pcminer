1a6e0fab208ba6babb607117c34285ea605993da
kas21e6 <54753774+kas21e6@users.noreply.github.com>
2024-02-24 11:11:19 -0800

    no message

--- src/Miner.py
+++ src/Miner.py
@@ -1,6 +1,43 @@
+import hashlib
+import struct
+import traceback
+
+
 class Miner:
-    @staticmethod
-    def calc_target_difficulty(hex_bits: str) -> bytes:
+    def __init__(self, hex_bits: str):
+        self.hex_bits = hex_bits
+        self.target = self._calc_target_difficulty(hex_bits)
+
+    def mine(self, header: object) -> bytes:
+        try:
+            nonce = header["nonce"]
+
+            while nonce < 0x100000000:
+                hash = self._double_hash(self._serialize_header(header, nonce))
+                print("Hash:", hash[::-1].hex())
+                print("With nonce", nonce)
+
+                if hash[::-1] < self.target:
+                    print("Success with nonce", nonce)
+                    break
+
+                nonce += 1
+        except Exception as e:
+            print("Error:", e)
+            traceback.print_exc()
+
+    def _double_hash(self, data: bytes) -> bytes:
+        return hashlib.sha256(hashlib.sha256(data).digest()).digest()
+
+    def _serialize_header(self, header: object, nonce: int) -> bytes:
+        return (
+            struct.pack("<L", header["version"])
+            + bytes.fromhex(header["prev_block"])[::-1]
+            + header["merkle_root"][::-1]
+            + struct.pack("<LLL", header["time"], int(header["bits"], 16), nonce)
+        )
+
+    def _calc_target_difficulty(self, hex_bits: str) -> bytes:
         """
         Decompress the target from a compact format.
         """
--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
+import hashlib
+import time
+from pprint import pprint
+
+from bitcoinlib.transactions import Transaction
 from bitcoinrpc.authproxy import JSONRPCException
 from rich import print
-from src.Miner import Miner
-from src.Connection import Connection
+
+from Connection import Connection
+from Miner import Miner
 
 
 class PcMiner:
@@ -15,10 +21,55 @@ class PcMiner:
             info = self.connection.getblocktemplate(
                 {"mode": "template", "rules": ["segwit"]}
             )
-            print(
-                "Connected to Bitcoin network: ",
-                Miner.calc_target_difficulty(info["bits"]).hex(),
+
+            # print(
+            #     "difficulty: ",
+            #     Miner.calc_target_difficulty(info["bits"]).hex(),
+            # )
+            # print(
+            #     "coinbase value: ",
+            #     info["coinbasevalue"],
+            # )
+            # print("previous block hash: ", info["previousblockhash"])
+
+            # Create the Coinbase Transaction
+            tx = Transaction(coinbase=False)
+            tx.add_input(
+                "0000000000000000000000000000000000000000000000000000000000000000",
+                0,
+                sequence=0xFFFFFFFF,
+                index_n=0xFFFFFFFF,
             )
+            tx.add_output(info["coinbasevalue"], "1runeksijzfVxyrpiyCY2LCBvYsSiFsCm")
+
+            # Calculate the Merkle Root
+            merkle_root = hashlib.sha256(
+                hashlib.sha256(bytes.fromhex(tx.raw_hex())).digest()
+            ).digest()
+
+            # Get current timestamp
+            timestamp = int(time.time())
+            print("timestamp: ", timestamp)
+
+            print("merkle root: ", merkle_root.hex())
+
+            # Create block header
+            header = {
+                "version": 2,
+                "prev_block": info["previousblockhash"],
+                "merkle_root": merkle_root,
+                "time": timestamp,
+                "bits": info["bits"],
+                "nonce": 0,
+            }
+
+            pprint(header)
+
+            # Mine the block
+            miner = Miner(info["bits"])
+
+            miner.mine(header)
+
         except JSONRPCException as json_exception:
             print("A JSON RPC Exception occurred: ", json_exception)
         except Exception as general_exception:
